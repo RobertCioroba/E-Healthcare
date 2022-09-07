da8ad5301f00f3b085c0127230477825563c28cb
Robert Cioroba <Robert.Mihai1@Dell.com>
2022-09-07 23:01:34 +0300

    User - C.R.U.D.

--- E-Healthcare/Data/DataContext.cs
+++ E-Healthcare/Data/DataContext.cs
@@ -8,5 +8,6 @@ namespace E_Healthcare.Data
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
         public DbSet<Account> Accounts { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
